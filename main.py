f8efe203371c3a65e91ef79525e55ed5a45526dd
Stefano <stefanocutristecco@gmail.com>
2024-09-22 20:57:29 -0300

    Crear matriz de ingresos brutos

--- main.py
+++ main.py
@@ -1,2 +1,45 @@
-
+import random
 print("Hola mundo")
+
+def crearMatrizAlicuotas():
+
+    matriz = []
+    filas = 23
+    columnas = 3
+
+    for i in range (filas):
+        fila = []
+        for j in range(columnas):
+            # Generar valores de alicoutas random y ordenarlos de mayor a menos
+            valores = [generarValoresAlicoutas() for _ in range(3)]
+            valores.sort()
+
+            #  Agregar los valores a la fila
+            fila = [valores[2], valores[1], valores[0]] 
+
+        matriz.append(fila)
+
+    
+    imprimirMatriz(matriz)
+
+
+# Funcion para generar valores de alicuotas ficticios
+def generarValoresAlicoutas():
+
+    alicouta = round(random.uniform(0.2, 7), 1)
+
+    return alicouta
+
+
+# Funcion para imprimir matriz - (A cambiar para que quede mejor visualmente)
+def imprimirMatriz(matriz):
+    print("Matriz de Alicuotas:")
+    print("-" * 30)
+    print("{:<10} {:<10} {:<10}".format("No inscripto", "Local", "Multilateral"))
+    print("-" * 30)
+    
+    for fila in matriz:
+        print("{:<10.1f} {:<10.1f} {:<10.1f}".format(fila[0], fila[1], fila[2]))
+
+
+crearMatrizAlicuotas()
