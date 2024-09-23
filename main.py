75169bf0d533cff2563c37f36e739e604ff2b4a6
ttutee <tutetomy2015@gmail.com>
2024-09-23 19:36:28 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,2 +1,52 @@
 
 print("Hola mundo")
+
+#funcion que pida al usuario los datos y los retorne al diccionario
+def obtener_datos_usuario():
+    # Crear el diccionario
+    datos = {}
+    
+    # Pedir datos al usuario
+    nombre = input("Ingrese su nombre: ")
+    edad = input("Ingrese su edad: ")
+    email = input("Ingrese su correo electrónico: ")
+    
+    # Almacenar los datos en el diccionario
+    datos["Nombre"] = nombre
+    datos["edad"] = edad
+    datos["email"] = email
+    
+    return datos
+
+# Llamar a la función y mostrar el resultado
+datos_usuario = obtener_datos_usuario()
+print("Datos del usuario:", datos_usuario)
+
+#tupla de provincias arg
+provincias_argentina = (
+    "Buenos Aires",
+    "Catamarca",
+    "Chaco",
+    "Chubut",
+    "Córdoba",
+    "Corrientes",
+    "Entre Ríos",
+    "Formosa",
+    "Jujuy",
+    "La Pampa",
+    "La Rioja",
+    "Mendoza",
+    "Misiones",
+    "Neuquén",
+    "Río Negro",
+    "Salta",
+    "San Juan",
+    "San Luis",
+    "Santa Cruz",
+    "Santa Fe",
+    "Santiago del Estero",
+    "Tierra del Fuego",
+    "Tucumán"
+)
+
+print(provincias_argentina)
