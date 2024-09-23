9fdc0cb07d61a979ec256d0a4a4255baf22fc7db
Bohle, Facundo Luis <53636017+bohledevs@users.noreply.github.com>
2024-09-23 20:00:03 -0300

    feat: funcion principal

--- main.py
+++ main.py
@@ -1,2 +1,12 @@
 
+
+def calcular_impuestos(datos_transaccion):
+
+    impuestos_aplicados = []
+    impuestos_nacionales = calcular_nacionales(datos_transaccion)
+    impuestos_provinciales = calcular_provinciales(datos_transaccion)
+    impuestos_aplicados.append(calcular_nacionales)
+    impuestos_aplicados.append(calcular_provinciales)
+    return impuestos_aplicados
+
 print("Hola mundo")
