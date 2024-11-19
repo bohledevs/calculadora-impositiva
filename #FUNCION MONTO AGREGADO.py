dadc54c938ebaf80e5df7f5c11677f089f68d99b
Brunogonz1 <brunogonzalezzzz1@gmail.com>
2024-11-19 16:40:20 -0300

    Funcion monto agregado
    map filter y  reduce

--- #FUNCION MONTO AGREGADO.py
+++ #FUNCION MONTO AGREGADO.py
@@ -1,34 +1,15 @@
-#FUNCION MONTO AGREGADO
+
 from functools import reduce
-
-# Lista de entrada
-datos = [
-    {
-        "titulo": "IVA",
-        "impuesto": 2010.5,
-        "tasa": 21
-    },
-    {
-        "titulo": "Ganancias",
-        "impuesto": 200.5,
-        "tasa": 0.2
-    },
-    {
-        "titulo": "Sin Impuesto",
-        "impuesto": 0,
-        "tasa": 10
-    }
-]
-
-#Se filtran elementos donde "impuesto" sea igual a 0
-resultados_filtrados = list(filter(lambda x: x["impuesto"] == 0, datos))
-
-#Usamos map para extraer solo el valor de "impuesto" de los resultados filtrados
-impuestos_filtrados = list(map(lambda x: x["impuesto"], resultados_filtrados))
-
-#Usamos reduce para sumar todos los valores del campo "impuesto" en la lista original
-total_impuestos = reduce(lambda acc, x: acc + x["impuesto"], datos, 0)
-
-# Imprimir resultados
-print("Impuestos filtrados (impuesto == 0):", impuestos_filtrados)
-print("Suma total de impuestos:", total_impuestos)
+ 
+def agregar_monto(datos):
+ 
+	#Se filtran elementos donde "impuesto" sea igual a 0
+	resultados_filtrados = list(filter(lambda x: x["impuesto"] == 0, datos))
+ 
+	#Usamos map para extraer solo el valor de "impuesto" de los resultados filtrados
+	impuestos_filtrados = list(map(lambda x: x["impuesto"], resultados_filtrados))
+ 
+	#Usamos reduce para sumar todos los valores del campo "impuesto" en la lista original
+	total_impuestos = reduce(lambda acc, x: acc + x["impuesto"], datos, 0)
+ 
+	return total_impuestos
