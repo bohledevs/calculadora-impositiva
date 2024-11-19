ed0e790face6da32e17820dcba9c6917e16283be
Brunogonz1 <brunogonzalezzzz1@gmail.com>
2024-11-19 19:08:54 -0300

    Update main.py
    llave

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ import login
 import csv
 from llaves import *
 from facturacion import imprimir_factura
+from functools import reduce
 
 # Tupla de provincias argentinas
 provincias_argentina = (
@@ -198,10 +199,25 @@ def calcular_iibb(monto, cf, provincia):
 
 # Amalgama los datos de la transaccion en una sola salida
 def obtener_resumen(datos_transaccion, impuestos_aplicados):
-    resumen = datos_transaccion.copy()
+    resumen = datos_transaccion.copy() 
     resumen[IMPUESTOS_APLICADOS] = impuestos_aplicados
+    resumen[IMPUESTOS_AGREGADOS] = impuestos_agregados(impuestos_aplicados)
     return resumen
 
+
+def agregar_monto(datos):
+ 
+	#Se filtran elementos donde "impuesto" sea igual a 0
+	resultados_filtrados = list(filter(lambda x: x["impuesto"] == 0, datos))
+ 
+	#Usamos map para extraer solo el valor de "impuesto" de los resultados filtrados
+	impuestos_filtrados = list(map(lambda x: x["impuesto"], resultados_filtrados))
+ 
+	#Usamos reduce para sumar todos los valores del campo "impuesto" en la lista original
+	total_impuestos = reduce(lambda acc, x: acc + x["impuesto"], datos, 0)
+ 
+	return total_impuestos
+    
 #Funcion para imprimir el resumen de una transaccion.
 def imprimir_resumen(resumen):
     # Extraemos los datos de la transacción
