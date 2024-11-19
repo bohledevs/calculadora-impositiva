62fedf3a0d6d44909c053962b108cfe23e53ad43
Bohle, Facundo Luis <53636017+bohledevs@users.noreply.github.com>
2024-11-19 13:29:39 -0300

    fix

--- facturacion.py
+++ facturacion.py
@@ -15,6 +15,7 @@ def imprimir_factura(resumen_transaccion):
         print(f"La factura ha sido guardada bajo el nombre: {nombre_factura}")
     finally:
         print("Facturación finalizada.")
+        
     return nombre_factura
 
 def obtener_nombre(resumen_transaccion):
@@ -27,34 +28,36 @@ def obtener_nombre(resumen_transaccion):
 def escribir_factura(resumen_transaccion, filename):
     usuario = resumen_transaccion[LLAVE_USUARIO]
 
-    with open(filename, "w", encoding="UTF-8") as file:
-        file.write("=" * 50 + "\n")
-        file.write(" " * 15 + "FACTURA B" + "\n")
-        file.write("=" * 50 + "\n")
-        file.write(f"Nombre: {resumen_transaccion[LLAVE_USUARIO][LLAVE_NOMBRE]}\n")
-        file.write(f"Domicilio: {usuario}\n")
-        file.write(f"Condición IVA: {resumen_transaccion[LLAVE_CF_IVA]}\n")
-        file.write(f"Fecha: {resumen_transaccion[LLAVE_FECHA]}\n")
-        file.write("-" * 50 + "\n")
-        file.write(f"{'Item':<20}{'Unidad (%)':<20}{'Monto ($)':<10}\n")
-        file.write("-" * 50 + "\n")
+    file = open(filename, "w", encoding="UTF-8")
+    file.write("=" * 50 + "\n")
+    file.write(" " * 15 + "FACTURA B" + "\n")
+    file.write("=" * 50 + "\n")
+    file.write(f"Nombre: {resumen_transaccion[LLAVE_USUARIO][LLAVE_NOMBRE]}\n")
+    file.write(f"Domicilio: {usuario}\n")
+    file.write(f"Condición IVA: {resumen_transaccion[LLAVE_CF_IVA]}\n")
+    file.write(f"Fecha: {resumen_transaccion[LLAVE_FECHA]}\n")
+    file.write("-" * 50 + "\n")
+    file.write(f"{'Item':<20}{'Unidad (%)':<20}{'Monto ($)':<10}\n")
+    file.write("-" * 50 + "\n")
 
-        total_neto = resumen_transaccion[LLAVE_MONTO]
-        total_impuesto = 0
+    total_neto = resumen_transaccion[LLAVE_MONTO]
+    total_impuesto = 0
 
-        file.write(f"{"Venta":<20}{100:<20}{total_neto:<10.2f}\n")
+    file.write(f"{"Venta":<20}{100:<20}{total_neto:<10.2f}\n")
 
-        for impuesto in resumen_transaccion[IMPUESTOS_APLICADOS]:
-            nombre = impuesto[LLAVE_TITULO]
-            tasa = impuesto[LLAVE_TASA]
-            monto = impuesto[LLAVE_IMPUESTO]
+    for impuesto in resumen_transaccion[IMPUESTOS_APLICADOS]:
+        nombre = impuesto[LLAVE_TITULO]
+        tasa = impuesto[LLAVE_TASA]
+        monto = impuesto[LLAVE_IMPUESTO]
             
-            total_impuesto += monto
+        total_impuesto += monto
             
-            file.write(f"{nombre:<20}{tasa:<20}{-monto:<10.2f}\n")
+        file.write(f"{nombre:<20}{tasa:<20}{-monto:<10.2f}\n")
 
         file.write("-" * 50 + "\n")
         file.write(f"{'Total Neto:':<40}${total_neto:<10.2f}\n")
         file.write(f"{'Impuestos:':<40}${total_impuesto:<10.2f}\n")
         file.write(f"{'Total a Pagar:':<40}${total_neto + total_impuesto:<10.2f}\n")
         file.write("=" * 50 + "\n")
+    
+    file.close()
